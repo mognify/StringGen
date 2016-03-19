c24ccbcabddd63528e059e3511cc1a92a690b081
mognify <levimogford@gmail.com>
2016-03-19 00:21:48 -0500

    added the random string generator
    I might further develop this program to include names and special characters as options

--- StringGen.java
+++ StringGen.java
@@ -1,1 +1,33 @@
- 
+import java.util.Scanner;
+
+public class StringGen {
+	public static void main(String[] args){
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+		
+		Integer length = 0;
+		if(args.length != 0){
+			length = Integer.valueOf(args[0]);
+		}else{
+			Scanner ui = new Scanner(System.in);
+			System.out.print("Length [7]: ");
+			String temp = ui.nextLine();
+			
+			if(temp.length() < 1)
+				length = 7;
+			else
+				length = Integer.valueOf(temp);
+		}
+		
+		
+		String output = "";
+		for(int i = 0; i < length; i++){
+			String letter = String.valueOf(alphabet.charAt((int)((Math.random()*25)+1)));
+			System.out.print(letter);
+			try {
+			    Thread.sleep(300);                 //1000 milliseconds is one second.
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}
+		}
+	}
+}
